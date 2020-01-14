a983fb4c2c5d3519d739ec7f22161de44320d8dc
agohorel <gohorelalex@gmail.com>
2020-01-14 16:17:29 -0500

    add content & basic styles to User component

--- github-user-card/src/components/User.js
+++ github-user-card/src/components/User.js
@@ -5,6 +5,7 @@ export const User = ({ user }) => {
   console.log(user);
   return (
     <UserCard>
+      <Avatar src={user.avatar_url}></Avatar>
       <h2>{user.login}</h2>
       <h3>{user.company}</h3>
       <h3>{user.location}</h3>
@@ -16,4 +17,21 @@ export const User = ({ user }) => {
 const UserCard = styled.div`
   background-color: #3c3c3c;
   padding: 1rem;
+  margin: 1rem;
+  color: #eee;
+
+  h2 {
+    font-size: 32px;
+    display: inline-block;
+    margin: 0 0 0 10px;
+  }
+
+  h3 {
+  }
+`;
+
+const Avatar = styled.img`
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
 `;
